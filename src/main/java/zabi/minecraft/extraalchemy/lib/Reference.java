61909a27ecbc94444c6833e3a78296f283b8e1b2
zabi94 <zabi94@gmail.com>
2018-02-15 22:57:15 +0100

    0.3.8-release

--- src/main/java/zabi/minecraft/extraalchemy/lib/Reference.java
+++ src/main/java/zabi/minecraft/extraalchemy/lib/Reference.java
@@ -4,9 +4,9 @@ public class Reference {
 
 	public static final String MID = "extraalchemy";
 	public static final String NAME = "Extra Alchemy";
-	public static final String VERSION = "0.3.7.1-release";
+	public static final String VERSION = "0.3.8-release";
 	
-	public static final int release = 42;
+	public static final int release = 43;
 	
 	public static final String PROXY_CLIENT = "zabi.minecraft.extraalchemy.proxy.ClientProxy";
 	public static final String PROXY_SERVER = "zabi.minecraft.extraalchemy.proxy.ServerProxy";
