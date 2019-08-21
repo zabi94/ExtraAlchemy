2099a139edaa474383e3508fc069e74d6021f5ad
Simone <zabi94@gmail.com>
2019-08-21 12:22:53 +0200

    Fix crash on servers

---
+++ src/main/java/zabi/minecraft/extraalchemy/items/MinervaMedal.java
@@ -0,0 +1,9 @@
+package zabi.minecraft.extraalchemy.items;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+public class MinervaMedal {
+	@ObjectHolder("minerva:supporter_medal")
+	public static final Item medal = null;
+}
