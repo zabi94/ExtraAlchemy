d24162e231cb5e16d9e90623375f24aa971a2456
Simone Zanin <zabi94@users.noreply.github.com>
2019-11-30 18:32:24 +0100

    Remove debug line

--- src/main/java/zabi/minecraft/extraalchemy/network/ServerPacketRegistry.java
+++ src/main/java/zabi/minecraft/extraalchemy/network/ServerPacketRegistry.java
@@ -3,7 +3,6 @@ package zabi.minecraft.extraalchemy.network;
 import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import zabi.minecraft.extraalchemy.client.network.ToggleMagnetismPacket;
 import zabi.minecraft.extraalchemy.entitydata.PlayerProperties;
-import zabi.minecraft.extraalchemy.utils.Log;
 
 public class ServerPacketRegistry {
 
@@ -12,7 +11,6 @@ public class ServerPacketRegistry {
 			boolean magnetismActive = buf.readBoolean();
 			ctx.getTaskQueue().execute(() -> {
 				((PlayerProperties) (Object) ctx.getPlayer()).setMagnetismEnabled(magnetismActive);
-				Log.d("Server: magnetism toggled for "+ ctx.getPlayer().toString());
 			});
 		});
 	}
