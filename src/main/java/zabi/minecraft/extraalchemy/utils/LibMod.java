03ea05c96faf183c34067849cc4b4e9bbbf68fe9
Simone Zanin <zabi94@users.noreply.github.com>
2020-07-26 15:26:33 +0200

    Utility method to create identifiers

--- src/main/java/zabi/minecraft/extraalchemy/utils/LibMod.java
+++ src/main/java/zabi/minecraft/extraalchemy/utils/LibMod.java
@@ -1,8 +1,14 @@
 package zabi.minecraft.extraalchemy.utils;
 
+import net.minecraft.util.Identifier;
+
 public class LibMod {
 
 	public static final String MOD_NAME = "Extra Alchemy";
 	public static final String MOD_ID = "extraalchemy";
 
+	public static Identifier id(String name) {
+		return new Identifier(MOD_ID, name);
+	}
+	
 }
