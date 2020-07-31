b3e33a5f02705708952cf0a3bf542a2c62bbde6b
Simone Zanin <zabi94@users.noreply.github.com>
2020-07-31 10:22:22 +0200

    Compiler can't infer stuff apparently. Let me be more explicit then

--- src/main/java/zabi/minecraft/extraalchemy/client/ModScreens.java
+++ src/main/java/zabi/minecraft/extraalchemy/client/ModScreens.java
@@ -1,13 +1,22 @@
 package zabi.minecraft.extraalchemy.client;
 
 import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
+import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry.Factory;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.text.Text;
 import zabi.minecraft.extraalchemy.screen.ModScreenHandlerTypes;
 import zabi.minecraft.extraalchemy.screen.potion_bag.PotionBagScreen;
+import zabi.minecraft.extraalchemy.screen.potion_bag.PotionBagScreenHandler;
 
 public class ModScreens {
 	
 	public static void init() {
-		ScreenRegistry.register(ModScreenHandlerTypes.POTION_BAG, (sh, pinv, titolo) -> new PotionBagScreen(titolo, sh, pinv));
+		ScreenRegistry.register(ModScreenHandlerTypes.POTION_BAG, new Factory<PotionBagScreenHandler, PotionBagScreen> () {
+			@Override
+			public PotionBagScreen create(PotionBagScreenHandler handler, PlayerInventory inventory, Text title) {
+				return new PotionBagScreen(title, handler, inventory);
+			}
+		});
 	}
 
 }
