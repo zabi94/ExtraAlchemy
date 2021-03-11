b292a93b660888997abc873a4eefe50b3b312d5e
Simone Zanin <zabi94@users.noreply.github.com>
2021-03-11 22:53:46 +0100

    Add ring recipes for effects without potions

--- src/main/java/zabi/minecraft/extraalchemy/crafting/CraftingRecipes.java
+++ src/main/java/zabi/minecraft/extraalchemy/crafting/CraftingRecipes.java
@@ -10,9 +10,12 @@ public class CraftingRecipes {
 	
 	public static SpecialRecipeSerializer<PotionVialRecipe> FILL_VIAL_SERIALIZER;
 	public static RecipeType<PotionVialRecipe> FILL_VIAL_RECIPE_TYPE;
-	
+
 	public static RecipeSerializer<PotionRingRecipe> RING_CRAFTING_SERIALIZER;
 	public static RecipeType<PotionRingRecipe> RING_CRAFTING_TYPE;
+	
+	public static RecipeSerializer<AlternativePotionRingRecipe> ALT_RING_CRAFTING_SERIALIZER;
+	public static RecipeType<AlternativePotionRingRecipe> ALT_RING_CRAFTING_TYPE;
 	
 	public static void init() {
 		FILL_VIAL_SERIALIZER = Registry.register(Registry.RECIPE_SERIALIZER, LibMod.id("fill_potion_vial"), new SpecialRecipeSerializer<PotionVialRecipe>(PotionVialRecipe::new));
@@ -20,6 +23,9 @@ public class CraftingRecipes {
 		
 		RING_CRAFTING_SERIALIZER = Registry.register(Registry.RECIPE_SERIALIZER, LibMod.id("potion_ring"), new PotionRingRecipe.Serializer());
 		RING_CRAFTING_TYPE = Registry.register(Registry.RECIPE_TYPE, LibMod.id("potion_ring"), new RecipeType<PotionRingRecipe>() {});
+		
+		ALT_RING_CRAFTING_SERIALIZER = Registry.register(Registry.RECIPE_SERIALIZER, LibMod.id("alt_potion_ring"), new AlternativePotionRingRecipe.Serializer());
+		ALT_RING_CRAFTING_TYPE = Registry.register(Registry.RECIPE_TYPE, LibMod.id("alt_potion_ring"), new RecipeType<AlternativePotionRingRecipe>() {});
 	}
 	
 }
