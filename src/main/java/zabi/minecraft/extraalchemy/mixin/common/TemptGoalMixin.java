20284e4707710f9abde1be4253b847ad01cd1622
zabi94 <11521878+zabi94@users.noreply.github.com>
2021-07-05 21:29:18 +0200

    Fix potion of the piper not applying the effect
    Mentioned in #111

--- src/main/java/zabi/minecraft/extraalchemy/mixin/common/TemptGoalMixin.java
+++ src/main/java/zabi/minecraft/extraalchemy/mixin/common/TemptGoalMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.TemptGoal;
 import net.minecraft.entity.player.PlayerEntity;
 import zabi.minecraft.extraalchemy.statuseffect.ModEffectRegistry;
@@ -16,8 +17,8 @@ public abstract class TemptGoalMixin {
 	@Shadow protected PlayerEntity closestPlayer;
 	
 	@Inject(method = "isTemptedBy", at = @At(value = "HEAD"), cancellable = true)
-	public void addPiperCheck(CallbackInfoReturnable<Boolean> cir) {
-		if (closestPlayer != null && closestPlayer.getStatusEffect(ModEffectRegistry.piper) != null) {
+	public void addPiperCheck(LivingEntity entity, CallbackInfoReturnable<Boolean> cir) {
+		if (entity != null && entity.getStatusEffect(ModEffectRegistry.piper) != null) {
 			cir.setReturnValue(true);
 		}
 	}
