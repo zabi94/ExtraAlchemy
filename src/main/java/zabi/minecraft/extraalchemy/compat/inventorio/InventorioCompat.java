c440ee2695ba564a63f09d6ad9ac455531d197f2
Simone Zanin <zabi94@users.noreply.github.com>
2022-10-23 14:27:47 +0200

    v1.9.0 - forgot one file

---
+++ src/main/java/zabi/minecraft/extraalchemy/compat/inventorio/InventorioCompat.java
@@ -0,0 +1,28 @@
+package zabi.minecraft.extraalchemy.compat.inventorio;
+
+import org.jetbrains.annotations.NotNull;
+
+import me.lizardofoz.inventorio.api.InventorioAPI;
+import me.lizardofoz.inventorio.api.InventorioAddonSection;
+import me.lizardofoz.inventorio.api.InventorioTickHandler;
+import me.lizardofoz.inventorio.player.PlayerInventoryAddon;
+import net.minecraft.item.ItemStack;
+import zabi.minecraft.extraalchemy.items.ModItems;
+import zabi.minecraft.extraalchemy.utils.LibMod;
+
+public class InventorioCompat {
+
+	public static void init() {
+		
+		InventorioAPI.registerInventoryTickHandler(LibMod.id("potion_ring_ticker"), new InventorioTickHandler() {
+			@Override
+			public void tick(@NotNull PlayerInventoryAddon invAddon, @NotNull InventorioAddonSection section, @NotNull ItemStack stack, int index) {
+				if (!invAddon.getPlayer().world.isClient && stack.getItem().equals(ModItems.POTION_RING)) {
+					ModItems.POTION_RING.inventoryTick(stack, invAddon.getPlayer().world, invAddon.getPlayer(), -1, false);
+				}
+			}
+		});
+		
+	}
+
+}
