7b31ca9be38ffc55f15bfe431ec9385653e4a703
Simone Zanin <zabi94@users.noreply.github.com>
2023-02-20 20:33:11 +0100

    Proper fix for random crashes on toggling the magnetism potion
    closes #140

--- src/main/java/zabi/minecraft/extraalchemy/client/network/ClientPacketRegistry.java
+++ src/main/java/zabi/minecraft/extraalchemy/client/network/ClientPacketRegistry.java
@@ -1,8 +1,7 @@
 package zabi.minecraft.extraalchemy.client.network;
 
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import zabi.minecraft.extraalchemy.network.S2C_Channels;
 
@@ -11,9 +10,7 @@ public class ClientPacketRegistry {
 	public static void init() {
 		
 		ClientPlayNetworking.registerGlobalReceiver(S2C_Channels.PLAY_CLICK_SOUND, (client, handler, buf,responseSender) -> {
-			
-			MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1f));
-			
+			client.execute(() -> client.player.playSound(SoundEvents.UI_BUTTON_CLICK.value(), SoundCategory.MASTER, 1, 1));
 		});
 		
 	}
